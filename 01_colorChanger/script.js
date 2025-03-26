3ac09aaae08d86c6878e1522b978c5e811df6afb
Dewanshu-Chirkhe <dewanshuchirkhe2004@gmail.com>
2025-03-26 23:48:07 +0530

    small JS project

---
+++ 01_colorChanger/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+button.forEach(function (button) {
+    // console.log(button);
+    button.addEventListener("click", function (event) {
+        body.style.backgroundColor = event.target.id;
+    });
+});
