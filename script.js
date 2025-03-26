540821ea37dbc655ed7b41066c5825b86630e84a
Dewanshu <167283813+Dewanshu-Chirkhe@users.noreply.github.com>
2025-03-26 23:36:50 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelectorAll('.button')
-const body = document.querySelector('body')
-
-button.forEach(function(button) {
-    // console.log(button);
-    button.addEventListener('click' , function(event) {
-        body.style.backgroundColor = event.target.id;
-    })
-})
